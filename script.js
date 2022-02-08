8d169708aa354cfad2377439b19e5d2f5fefcd7f
NathLo <thyalan.aurlence@gmail.com>
2022-02-08 19:28:21 -0400

    scripts added

---
+++ script.js
@@ -0,0 +1,8 @@
+const img = document.querySelector("img");
+const icons = document.querySelector(".icons");
+         
+img.onclick = function(){
+           
+    this.classList.toggle("active");
+    icons.classList.toggle("active");
+}
